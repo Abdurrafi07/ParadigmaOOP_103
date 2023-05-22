8dd9bf80c4e1f9a382a91e0d0d5723eb1c98872c
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 21:07:16 +0700

    Constructor class ibu

--- mainIA/Header.h
+++ mainIA/Header.h
@@ -6,6 +6,11 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;
 
-
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu(){
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
 };
 #endif // !IBU_h
