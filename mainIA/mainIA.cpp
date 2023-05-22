f8a65dc3babf762d6ff73482c3e393ae2cf61961
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 21:29:28 +0700

    Constructor mainIA

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -5,4 +5,17 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main
+int main() {
+	ibu* varIbu = new ibu("Yul");
+	ibu* varIbu2 = new ibu("Naf");
+	anak* varAnak1 = new anak("Yono");
+	anak* varAnak2 = new anak("Nonoy");
+	anak* varAnak3 = new anak("Loyo");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+
+}
