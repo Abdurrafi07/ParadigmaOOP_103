3e8633ad0e91304956d2cfd31f8e6c0fdde9e82a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 11:30:23 +0700

    Membuat class orang

---
+++ Pewarisan/Pewarisan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+	public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+};
