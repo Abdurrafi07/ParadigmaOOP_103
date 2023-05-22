7b82897b3205d49ebcd282dc4184b5467d5ab10f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 11:39:18 +0700

    Membuat class pelajar

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -18,3 +18,15 @@ class orang {
 		return a + b;
 	}
 };
+
+class pelajar : public orang {
+	public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah):
+	orang(pNama),
+	sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+		}
+
+};
