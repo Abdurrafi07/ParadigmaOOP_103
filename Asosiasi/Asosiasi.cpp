43879698d3557777494648f07494ea39732cc6ec
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 20:05:06 +0700

    Menambahkan constructor class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -7,5 +7,10 @@ class pasien {
 public:
 	string nama;
 	vector<dokter>daftar_dokte;
-
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
 };
