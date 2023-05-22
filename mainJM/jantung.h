b4900e74008b9159a6f938776480c5c2f04fc124
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 21:36:57 +0700

    Membuat class jantung

---
+++ mainJM/jantung.h
@@ -0,0 +1,15 @@
+#ifndif JANTUNG_H
+#define JanTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
