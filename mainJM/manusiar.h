f975f512ab62e0f3203f68c31357a097095e0642
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-22 21:42:11 +0700

    Membuat class manusia

--- mainJM/manusiar.h
+++ mainJM/manusiar.h
@@ -1,2 +1,18 @@
-#ifndif
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName)
+		:name(pName) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << " mati\n";
+	}
+};
 #endif
